8b0d92cadb48a4f4a0fc01934c9ad12074b28401
Logan Aaron <loganaaron16@gmail.com>
2022-09-13 11:25:01 -0500

    completed app file with routes

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,24 +1,29 @@
-const path = require("path");
 
-require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
-const express = require("express");
-const cors = require("cors");
+const path = require('path')
 
-const errorHandler = require("./errors/errorHandler");
-const notFound = require("./errors/notFound");
-const reservationsRouter = require("./reservations/reservations.router");
-const { urlencoded } = require("express");
+require('dotenv').config({ path: path.join(__dirname, '..', '.env') })
 
-const app = express();
+const express = require('express')
+const cors = require('cors')
 
-app.use(cors());
-app.use(express.json());
-app.use(urlencoded({ extended: true }));
+const errorHandler = require('./errors/errorHandler')
+const notFound = require('./errors/notFound')
+const reservationsRouter = require('./reservations/reservations.router')
+const tablesRouter = require('./tables/tables.router')
 
-app.use("/reservations", reservationsRouter);
+const app = express()
 
-app.use(notFound);
-app.use(errorHandler);
+app.use(cors())
+app.use(express.json())
 
-module.exports = app;
+app.use('/reservations', reservationsRouter)
+app.use('/tables', tablesRouter)
+
+app.use(notFound)
+app.use(errorHandler)
+
+module.exports = app
+
+
+
